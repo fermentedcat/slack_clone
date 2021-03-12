7a4ebad6fd70f04c282452c87b73b0ec1e5d3b69
Maja Thunberg <majaneko@gmail.com>
2021-03-12 18:07:36 +0100

    Config passport

---
+++ config/passport.js
@@ -0,0 +1,44 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+module.exports = function (passport) {
+    passport.use(new LocalStrategy(
+        {
+            usernameField: 'email'
+        },
+        function (username, password, done) {
+            User.findOne({ email: username }, function (error, user) {
+                if (error) {
+                    return done(error)
+                }
+
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect email or password.'})
+                }
+
+                bcrypt.compare(password, user.password, (error, isMatch) => {
+                    if (error) {
+                        throw error
+                    }
+                    if (isMatch) {
+                        return done(null, user)
+                    } else {
+                        return done(null, false, { message: 'Incorrect email or password.'})
+                    }
+                })
+            })
+                .catch(error => console.log(error))
+        }
+    ))
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (error, user) => {
+            done(error, user)
+        })
+    })
+}
