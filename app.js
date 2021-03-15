c109cd9e382f1c95923d3ead8e92c6c0467f527a
Maja Thunberg <majaneko@gmail.com>
2021-03-16 00:43:24 +0100

    Integrate socket.io

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 require('dotenv').config()
 
-const express = require('express')
-const app = express()
-const router = express.Router()
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/slack_clone')
-    .then(() => { console.log('connected to db')})
-    .catch(error => console.log("error"))
+.then(() => { console.log('connected to db')})
+.catch(error => console.log("error"))
 
 const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const router = express.Router()
+
+
 const flash = require('connect-flash')
 const session = require('express-session')
 const expressEjsLayouts = require('express-ejs-layouts')
+const fileUpload = require('express-fileupload')
+
 //// passport
 const passport = require('passport')
 require('./config/passport')(passport)
-
-
+// const sharedsession = require('express-socket.io-session')
 
 
 app.set('view engine', 'ejs')
@@ -25,6 +34,7 @@ app.use(expressEjsLayouts)
 app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload({ createParentPath: true }))
 
 
 //// SESSIONS
@@ -33,6 +43,12 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+
+/* io.use(
+    sharedsession(session, {
+        autoSave: true
+    })
+) */
 
 //// PASSPORT
 app.use(passport.initialize())
@@ -56,8 +72,50 @@ app.use('/channels', require('./routes/channels'))
 app.use('/', require('./routes/index'))
 
 
+io.on('connection', socket => {
+    console.log("New WS Connection");
+    let room = ""
+    let user = ""
+
+    socket.on('joinRoom', ({ user_id, room_id }) => {
+        room = room_id
+        user = user_id
+        socket.join(room)
+        console.log(`Joined room ${room}`);
+    })
+
+    socket.on('chat message', message => {
+        console.log("user");
+        console.log(`message received: ${message}`);
+        io.to(room).emit('chat message', message)
+    })
+
+    socket.on('reply message', message => {
+        console.log(`reply received: ${message}`);
+        io.to(room).emit('reply message', message)
+    })
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to s1ack!')
+    /* socket.on('login', function(userdata) {
+        socket.handshake.session.userdata = userdata
+        socket.handshake.session.save()
+        console.log(socket.handshake.session);
+    }) */
+
+    // Broadcast to other user that a new user has connected
+    socket.broadcast.emit('message', 'A user has joined the chat.')
+
+
+    socket.on('disconnect', () => {
+        // Emit to all users when someone leaves
+        io.emit('message', 'A user has left the chat')
+    })
+
+})
 
 
 
+const PORT = 3000 || process.env.PORT
 
-app.listen(3000)
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
