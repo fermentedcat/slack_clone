335d1eb295aacccb3233caaad1e04f8a26fbb2ef
Maja Thunberg <majaneko@gmail.com>
2021-03-17 15:57:39 +0100

    Add edit profile modal and function

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,20 +1,14 @@
 document.addEventListener('DOMContentLoaded', e => {
     const profile_pic = document.getElementById('profile_pic')
-    const image_btn = document.getElementById('profile_pic')
+    const image_btn = document.getElementById('image_btn')
     const user_id = profile_pic.getAttribute('data-id')
 
     // Check for uploaded profile pic
-    const fs = require('fs')
-    let image = "/public/images/profile/default.png"
-    const dir = `/public/images/profile/${user_id}`
-    fs.readFile(dir, (err, file) => {
-        if (file) {
-            image = `/public/images/profile/${user_id}`
-        }
-    })
+    
 
     image_btn.addEventListener('click', e => {
         const div = document.createElement('div')
+        console.log(e);
         div.innerHTML = `
         <form 
             action="/users/upload-profile-pic" 
@@ -24,5 +18,29 @@ document.addEventListener('DOMContentLoaded', e => {
                 <input type="submit" value="Save">
         </form>
         `
+        image_btn.parentNode.appendChild(div)
     })
 })
+
+function editUserInfo() {
+    const data = {
+        first_name: document.getElementById('first_name').value,
+        last_name: document.getElementById('last_name').value,
+        username: document.getElementById('username').value,
+        phone: document.getElementById('phone').value,
+        fav_pizza: document.getElementById('fav_pizza').value,
+        occupation: document.getElementById('occupation').value,
+    }
+    fetch(`/users/edit`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(user => {
+        console.log(user);
+        window.location.href = `/users/${user._id}`
+    })
+}
