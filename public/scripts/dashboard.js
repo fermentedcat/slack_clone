aa12b750c3c0ced85e11dc0a046fda34cf97d2db
Maja Thunberg <majaneko@gmail.com>
2021-03-17 16:05:11 +0100

    Emit new channel, make function addNewChannel

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,8 +1,17 @@
+const socket = io()
+
+socket.on('new channel', channel => {
+    addNewChannel(channel)
+})
+
 function createChannel() {
+    const private = document.getElementById('private').checked
+    
 
     const channel = {
                 name: document.getElementById('channel_name').value,
                 description: document.getElementById('channel_description').value,
+                private: private
             }
     console.log(channel);
 
@@ -13,8 +22,20 @@ function createChannel() {
         },
         body: JSON.stringify(channel)
     })
-    .then(res => {})
-    .then(data => {
+    .then(res => res.json())
+    .then(channel => {
+        if (!private) {
+            socket.emit('new channel', channel)
+        }
         window.location.href = '/dashboard'
     })
+}
+
+function addNewChannel(channel) {
+    const ul = document.getElementById('channels')
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <a href="/channels/${channel._id}">${channel.name}</a>
+    `
+    ul.appendChild(li)
 }
