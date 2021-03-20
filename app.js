7f0ff2653e757325085fa468d82e76c80caf7b06
Maja Thunberg <majaneko@gmail.com>
2021-03-21 00:08:49 +0100

    Remove redundant code from socket section

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 require('dotenv').config()
 
-
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/slack_clone')
     .then(() => { console.log('connected to db')})
     .catch(error => console.log("error"))
 
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
+const path      = require('path')
+const http      = require('http')
+const express   = require('express')
+const socketio  = require('socket.io')
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app       = express()
+const server    = http.createServer(app)
+const io        = socketio(server)
 
-const router = express.Router()
+const router    = express.Router() //* behövs den?
 
 
-const flash = require('connect-flash')
-const session = require('express-session')
+const flash     = require('connect-flash')
+const session   = require('express-session')
 const expressEjsLayouts = require('express-ejs-layouts')
 const fileUpload = require('express-fileupload')
+
 const { loginUser,
     logoutUser,
     getCurrentUser,
@@ -32,9 +32,7 @@ const { formatMessage } = require('./config/format.js')
 
 //// passport
 const passport = require('passport')
-const User = require('./models/user')
 require('./config/passport')(passport)
-// const sharedsession = require('express-socket.io-session')
 
 
 app.set('view engine', 'ejs')
@@ -57,12 +55,6 @@ app.use(session({
     }
 }))
 
-/* io.use(
-    sharedsession(session, {
-        autoSave: true
-    })
-) */
-
 //// PASSPORT
 app.use(passport.initialize())
 app.use(passport.session())
@@ -79,71 +71,68 @@ app.use((req, res, next) => {
 })
 
 
-// routes
+//// routes
 app.use('/users', require('./routes/users'))
 app.use('/channels', require('./routes/channels'))
+app.use('/posts', require('./routes/posts'))
 app.use('/', require('./routes/index'))
 
 
 io.on('connection', socket => {
     console.log("New WS Connection");
     let room = ""
-    let current_user = {}
     
+    //// Store user info of online user
     socket.on("online", user => {
         loginUser(user, socket.id)
-        console.log(`socket id upon login: ${socket.id}`);
         io.emit("online", user)
     })
 
     //// join room
-    socket.on('joinRoom', ({ user_id, room_id }) => {
+    socket.on('joinRoom', room_id => {
         room = room_id
         socket.join(room)
     })
 
     //// message
-    socket.on('chat message', message => {
+    socket.on('chat message', message => { //* döp om till new post
         const current_user = getCurrentUser(socket.id)
-        const online = getOnlineUsers()
-        console.log(online);
-        console.log(socket.id);
-        const data = {
-            post_data: formatMessage(message, current_user),
-            current_user: current_user
-        }
+        const data = formatMessage(message, current_user)
         io.to(room).emit('chat message', data)
     })
 
     //// reply
-    socket.on('reply message', message => {
-        io.to(room).emit('reply message', message)
+    socket.on('reply message', reply_data => { //* döp om till new reply
+        const current_user = getCurrentUser(socket.id)
+        const data = {
+            reply_data: formatMessage(reply_data.message, current_user),
+            post_id: reply_data.post_id
+        }
+        console.log(data.reply_data);
+        io.to(room).emit('reply message', data) //* lägg till koden från chat message?
     })
 
-    // Welcome current user
-    socket.emit('message', 'Welcome to s1ack!')
-    /* socket.on('login', function(userdata) {
-        socket.handshake.session.userdata = userdata
-        socket.handshake.session.save()
-        console.log(socket.handshake.session);
-    }) */
+    //// Remove deleted post OR reply
+    socket.on("delete msg", msg_id => {
+        io.to(room).emit("delete msg", msg_id)
+    })
+    
+    //// Remove deleted post OR reply
+    socket.on("update msg", data => {
+        io.to(room).emit("update msg", data)
+    })
 
-    // Broadcast to other user that a new user has connected
-    socket.broadcast.emit('message', 'A user has joined the chat.')
-
+    //// New channel created
     socket.on('new channel', channel => {
         io.emit('new channel', channel)
     })
 
     socket.on('disconnect', () => {
-        console.log("let's log out");
-        // Emit to all users when someone leaves
         const current_user = getCurrentUser(socket.id)
-        const online_users = logoutUser(socket.id)
         socket.broadcast.emit("offline", current_user)
+        logoutUser(socket.id)
         // socket.removeAllListeners()
     })
-
 })
 
 
