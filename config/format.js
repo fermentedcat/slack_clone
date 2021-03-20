bac1d4e338330e0516c5b8770ab9f07700591973
Maja Thunberg <majaneko@gmail.com>
2021-03-21 00:07:25 +0100

    Use new Date for formatting new messages

--- config/format.js
+++ config/format.js
@@ -1,26 +1,26 @@
 function formatMessage(message, current_user) {
     console.log("message:");
     console.log(message);
+    const date = new Date()
     const message_data = {
         _id: message._id,
         author: current_user._id,
         username: current_user.username,
         published: message.published,
-        date: formatDate(message.published).date,
-        time: formatDate(message.published).time,
+        date: formatDate(date).date,
+        time: formatDate(date).time,
         content: message.content
     }
     return message_data
 }
 
 function formatDate(date) {
-    /* const formattedDate = date.toLocaleDateString('en-GB', {
+    const formattedDate = date.toLocaleDateString('en-GB', {
         day: 'numeric', month: 'long', year: 'numeric'
       }).replace(/ /g, ' ');
     const formattedTime = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
 
-    return { date: formattedDate, time: formattedTime } */
-    return {time: Date.now(), date: Date.now()}
+    return { date: formattedDate, time: formattedTime }
 }
 
 module.exports = {
